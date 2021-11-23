a28c3268a1c7115e8da72ff88990431d454a7fc8
Alwin <nedrum@gmail.com>
2021-11-23 11:27:57 +0100

    Set up the basic chord grid

---
+++ src/App.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	
+
+  let chord = { 
+	  chordName: "Am", 
+	  chordKey: "A minor", 
+	  harmonicFunction: "i",
+	  scale: ["a", "b", "c", "d", "e", "f", "g"],
+	   strings: [
+		{openString: "e", stringNum: 1, fret: "0", noteName: "e", harmony: "5", note: "e3"},
+		{openString: "b", stringNum: 2, fret: "1", noteName: "c", harmony: "3", note: "c3" },
+		{openString: "g", stringNum: 3, fret: "2", noteName: "a", harmony: "R", note:"a2" },
+		{openString: "d", stringNum: 4, fret: "2", noteName: "e", harmony: "5",  note:"e2"},
+		{openString: "a", stringNum: 5, fret: "0", noteName: "a", harmony: "R", note: "a1"},
+		{openString: "e", stringNum: 6, fret: "x", noteName: "-", harmony: "-", note: ""}
+					]
+    }
+
+
+
+</script>
+
+<div class="card">
+  <h2>Harmony Grid</h2>
+	<p class="key">Key: {chord.chordKey}</p>
+
+	<div class="grid-card">
+  	  
+	  <table class="chord-grid">
+		<tr>
+			<td class="frets">{chord.strings[0].fret}</td>
+			<td class="notes">{chord.strings[0].noteName}</td>
+			<td class="harmony">{chord.strings[0].harmony}</td>
+		</tr>
+		<tr>
+			<td class="frets">{chord.strings[1].fret}</td>
+			<td class="notes">{chord.strings[1].noteName}</td>
+			<td class="harmony">{chord.strings[1].harmony}</td>
+		</tr>
+		<tr>
+			<td class="frets">{chord.strings[2].fret}</td>
+			<td class="notes">{chord.strings[2].noteName}</td>
+			<td class="harmony">{chord.strings[2].harmony}</td>
+		</tr>
+		<tr>
+			<td class="frets">{chord.strings[3].fret}</td>
+			<td class="notes">{chord.strings[3].noteName}</td>
+			<td class="harmony">{chord.strings[3].harmony}</td>
+		</tr>
+		<tr>
+			<td class="frets">{chord.strings[4].fret}</td>
+			<td class="notes">{chord.strings[4].noteName}</td>
+			<td class="harmony">{chord.strings[4].harmony}</td>
+		</tr>
+		<tr>
+			<td class="frets">{chord.strings[5].fret}</td>
+			<td class="notes">{chord.strings[5].noteName}</td>
+			<td class="harmony">{chord.strings[5].harmony}</td>
+		</tr>
+		
+	</table>
+	<p class="chord-name">{chord.chordName}</p>
+	<p class="chord-function">{chord.harmonicFunction}</p>
+		
+</div>
+
+</div>
+
+<style>
+* {
+	margin: 0;
+}
+
+.grid-card {
+	/* border: solid 1px salmon; */
+	max-width: 150px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.chord-grid {
+	/* border: solid 1px salmon; */
+}
+
+.chord-grid td  {
+	/* border: solid 1px salmon; */
+	width: 2em;
+	padding: 0;
+	margin: 0;
+	text-align: center;
+	line-height: 1;
+
+}
+
+
+.frets {
+	font-weight: bold;
+}
+.notes {
+	font-style: italic;
+}
+.chord-name {
+	font-size: 1.2rem;
+	font-weight: bold;
+	margin: 0;
+}
+
+</style>
