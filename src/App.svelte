304fbe2dfb6f75252bc840e4e69de751f787f28c
Alwin <nedrum@gmail.com>
2021-11-24 12:57:32 +0100

    Added functions to make musical spectre + chords

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,80 @@
 <script lang="ts">
-	
 
-  let chord = { 
+import { getMode, makeMusicalSpectre, makeChord }  from './Functions.svelte'
+
+
+
+const chromaticScales ={
+	sharps: ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a","a#","b"],
+	flats: ["c", "db", "d", "eb", "e", "f", "gb", "g", "ab", "a","bb","b"]
+}
+
+ let majorScales = {
+	 C: ["c", "d", "e", "f", "g", "a", "b"],
+	 G: [ "g", "a", "b","c", "d", "e", "f#"],
+	 D: [ "d", "e", "f#", "g", "a", "b", "c#"],
+	 A: [ "a", "b", "c#", "d", "e", "f#", "g"],
+	 E: [ "e", "f#", "g", "a", "b", "c#", "d"] 
+	
+ }
+ 
+
+
+
+ let guitarNeck = {
+  firstString: [ 
+	  
+	  { fret: 0, noteName: "e", note: "E3" },
+	  { fret: 1, noteName: "f", note: "F3" },
+	  { fret: 2, noteName: "f#", note: "F#3" },
+	  { fret: 3, noteName: "g", note: "G3" },
+	  { fret: 4, noteName: "g#", note: "G#3" },
+	  { fret: 5, noteName: "a", note: "A3" },
+	  { fret: 6, noteName: "a#", note: "A#3" },
+	  { fret: 7, noteName: "b", note: "B3" },
+	  { fret: 8, noteName: "c", note: "C4" },
+	  { fret: 9, noteName: "c#", note: "C#4" },
+	  { fret: 10, noteName: "d", note: "D4" },
+	  { fret: 11, noteName: "d#", note: "D#4" },
+	  { fret: 12, noteName: "e", note: "E4" },
+	  { fret: 13, noteName: "f", note: "F4" },
+	  { fret: 14, noteName: "f#", note: "F#4" },
+	  { fret: 15, noteName: "g", note: "G4" },
+	  { fret: 16, noteName: "g#", note: "G#4" },
+	  { fret: 17, noteName: "a", note: "A4" },
+	  { fret: 18, noteName: "a#", note: "A#4" },
+	  { fret: 19, noteName: "b", note: "B4" },
+	  { fret: 20, noteName: "c", note: "C5" },
+	  { fret: 21, noteName: "c#", note: "C#5" },
+	  { fret: 22, noteName: "d", note: "D5" },
+	  { fret: 23, noteName: "d#", note: "D#5" },
+	  { fret: 24, noteName: "e", note: "E5" },
+
+	]
+ }
+
+
+
+
+
+
+
+let chromatic = getMode(chromaticScales.sharps, 5)
+
+let twoOctaves = [...chromatic]
+console.log(
+
+twoOctaves
+	
+)
+
+ const chord = { 
 	  chordName: "Am", 
-	  chordKey: "A minor", 
+	  chordKey: "A natural minor",
+	  chordTones: ["a", "c", "e"], 
 	  harmonicFunction: "i",
 	  scale: ["a", "b", "c", "d", "e", "f", "g"],
+	  diatonicChords: ["i", "iib5", "III", "iv", "v", "bVI", "bVII" ],
 	   strings: [
 		{openString: "e", stringNum: 1, fret: "0", noteName: "e", harmony: "5", note: "e3"},
 		{openString: "b", stringNum: 2, fret: "1", noteName: "c", harmony: "3", note: "c3" },
@@ -16,9 +85,41 @@
 					]
     }
 
+class GuitarChord {
+	chordName : string
+	chordKey  : string
+	harmonicFunction : string
+	strings : {}[]
+	scale : string[]
 
 
+
+	constructor(chordName, chordKey, harmonicFunction, scale, strings ) {
+		this.chordName = chordName
+		this.chordKey = chordKey
+		this.harmonicFunction = harmonicFunction
+		this.strings = strings
+		this.scale = scale
+		
+
+	}
+}
+
+// let aMinor = new GuitarChord("Am", "A natural minor", "i", chord.scale, chord.strings)
+// console.log(aMinor)
 </script>
+
+<div class="guitarneck">
+	{#each guitarNeck.firstString as {fret, noteName, note}, i }
+  <li>
+	  
+	<ul>{note}</ul>
+
+  </li>
+  	
+  {/each}
+
+</div>
 
 <div class="card">
   <h2>Harmony Grid</h2>
@@ -69,6 +170,7 @@
 <style>
 * {
 	margin: 0;
+	padding: 0;
 }
 
 .grid-card {
@@ -106,4 +208,10 @@
 	margin: 0;
 }
 
+li {
+	display: inline-block;
+
+}
+
+ul {padding: .5em .2em;}
 </style>
---
+++ src/Functions.svelte
@@ -0,0 +1,39 @@
+<script lang="ts" context="module">
+  //turns scale into mode
+  export function getMode( scale: string[], scaleDegree: number) {
+	let mode = []
+	let notes = scale.slice(0,scaleDegree - 1)
+    let flippedNotes = scale.slice(- scale.length + (scaleDegree - 1))
+    mode = notes.map(note => {flippedNotes.push(note)})
+
+    return flippedNotes
+} 
+
+// Takes Scales and Transposes to all all octaves
+export function makeMusicalSpectre(scale: string[]) {
+  let octaves= [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+  let fullChromatic = []
+	octaves.map( octave => {
+    	scale.map(note => {
+    	fullChromatic.push(note + octaves[octave])})
+	})
+	return fullChromatic
+}
+
+// Takes a scale and stacks it into chords (tones by thids)
+export function makeChord(scale : string[], notes : number ) {
+	let scaleInThids = []
+	let upperExtensions = []
+	scale.filter((note, index, array) => {
+		if (index % 2 === 0) {
+			scaleInThids.push(note)
+		} else upperExtensions.push(note) 
+	})
+	return scaleInThids.concat(upperExtensions).splice(0, notes)
+}
+
+
+
+
+</script>
+
