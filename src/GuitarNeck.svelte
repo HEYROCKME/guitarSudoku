c7c4a39e41ecfff2cfa29fddf436ee2202402636
Alwin <nedrum@gmail.com>
2021-12-08 16:46:57 +0100

    added string and fret numbers

--- src/GuitarNeck.svelte
+++ src/GuitarNeck.svelte
@@ -1,16 +1,34 @@
 <script lang="ts" >
-  import  { guitarNeck } from './NeckMaker.svelte'
+  import  { guitarNeck, frets } from './NeckMaker.svelte'
+
+  let displayFrets = []
+  let fretCount = 0
+
+  while (fretCount < frets +1) {
+    displayFrets.push(fretCount)
+    fretCount +=1
+  }
 
 </script>
 
 <h1>Guitar neck</h1>
 <table>
   {#each guitarNeck as fretboard, i }
-    <tr>{#each fretboard as note}
+  
+  <tr>
+      <td>string{i+1}</td>
+   
+   {#each fretboard as note}
         <td class={note.classCSS}>{note.noteName}</td>
       {/each}
     </tr>
   {/each}
+  <tr>
+    <td></td>
+    {#each displayFrets as frets, i}
+    <td>{i}</td>
+    {/each}
+  </tr>
 </table>
 
 <style>
