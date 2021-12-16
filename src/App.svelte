ba32069c801a35b39731ce97e6e4033bb65c413d
Alwin <nedrum@gmail.com>
2021-12-16 18:20:47 +0100

    removed "commented-out" code

--- src/App.svelte
+++ src/App.svelte
@@ -2,69 +2,13 @@
 import HarmonyGrid from './HarmonyGrid.svelte'
 import GuitarNeck from './GuitarNeck.svelte'
 import DiatonicHarmony from './DiatonicHarmony.svelte';
-// import NoteSelector from './noteSelector.svelte';
-// import {makeScale} from './ScaleToChord.svelte';
-// import {getMode, makeDiatonicChords} from './Functions.svelte'
-// import { chromaticScales, romanNumerals } from "./Constants.svelte";
-
-// let cMajor = makeScale(chromaticScales.flats, "major")
-// let cMinor = makeScale(chromaticScales.flats, "major")
-// let fMinor = makeScale(getMode(chromaticScales.flats, 6), "minor")
-
-// let chromatic = getMode(chromaticScales.sharps, 6)
-// let fLydian = getMode(cMajor, 4)
-
-
-// let cMajorChords = makeDiatonicChords(cMajor, 4)
-// console.log(cMajorChords);
-
 
 </script>
 
 <h1>Guitar Sudoku</h1>
 
 <DiatonicHarmony/>
-
-<!-- <h2>the makeScale() function</h2>
-
-<p>C Major: {cMajor}</p>
-<p>C minor: {cMinor}</p>
-<p>chromatic {chromatic}</p>
-<p>F lydian mode: {fLydian}</p>
-<p>F minor: {fMinor}</p> 
-
-<div class="docs">
-
-	<h2>Make Diatonic chords</h2>
-	<p>
-		The <b>makeDiatonicChords()</b> function takes scale (Array) and notesInChord (number) 	as arguments.This function runs a map method on the scale array, wich passes the getMode() function, with scale and the "index", added by 1, as arguments thru the makeChord() function, together with "noteInChord". The return is a nested a array populated by arrays of note names of each chord.  
-	
-	</p>
-</div>
-
-{ #each cMajorChords as chord, o }
- <div class="diatonic-chord" >
-	 <p class="roman-numerals">{romanNumerals.majorScale[o]}</p>
-   { #each chord as note, i }
-    <p 
-	class="note" 
-	style="background-color: hsl({(i * 51) + (o * 153)}, 50%, 65%)"
-	>
-	{note}, </p>
-  { /each }
-</div>
-{ /each } -->
-
-
 <GuitarNeck/>
-<!-- <div class="selectors">
-	<NoteSelector kind="pink"/> 
-	<NoteSelector kind="skyblue"/>
-	<NoteSelector kind="salmon"/>
-	<NoteSelector kind="lightgreen"/>
-	<NoteSelector kind="aquamarine"/>
-	<NoteSelector kind="goldenrod"/>
-</div> -->
 
 
 <h1>Harmony</h1>
