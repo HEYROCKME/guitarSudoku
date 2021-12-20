963ae54f01c6a817a03b41940dbea636e0521130
Alwin <nedrum@gmail.com>
2021-12-20 12:11:35 +0100

    Added scaleType prop to MakeScale

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@ import MakeScale from './MakeScale.svelte'
 
 <h1>Guitar Sudoku</h1>
 
-<MakeScale noteName="e" />
+<MakeScale noteName="e" scaleType="major" />
 <DiatonicHarmony />
 <GuitarNeck />
 
